026345e2bcc659ee938c35cb862ccf79e2fcf911
Oscar McAuley <oscar@Oscars-MacBook-Air.local>
2025-03-28 21:43:24 +0000

    Added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,
     proxy: {
-      '/api': {
-          target: 'http://choices-api:5000/',
-          secure: false,      
-      }
-    }
-  }
-})
+      "/api": {
+        target: "http://choices-api:5000/",
+        secure: false,
+      },
+    },
+  },
+});
